50e490ed31d598ce0c6bb4f989e7644265f867fc
Alexander Makarov <sam@rmcreative.ru>
2021-02-09 14:00:24 +0300

    Fix #43: Implement composite matcher

---
+++ src/PathMatcher/CompositeMatcher.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Files\PathMatcher;
+
+use InvalidArgumentException;
+use function get_class;
+use function is_object;
+
+/**
+ * Composite matcher allows combining several matchers.
+ */
+final class CompositeMatcher implements PathMatcherInterface
+{
+    private bool $matchAny;
+
+    /**
+     * @var array PathMatcherInterface[]
+     */
+    private array $matchers;
+
+    private function __construct(bool $matchAny, array $matchers)
+    {
+        foreach ($matchers as $matcher) {
+            if (!$matcher instanceof PathMatcherInterface) {
+                $type = $this->getVariableType($matcher);
+                $message = sprintf(
+                    'Matchers should contain instances of \Yiisoft\Files\PathMatcher\PathMatcherInterface, %s given.',
+                    $type
+                );
+                throw new InvalidArgumentException($message);
+            }
+        }
+        $this->matchers = $matchers;
+        $this->matchAny = $matchAny;
+    }
+
+    /**
+     * Get an instance of composite matcher that gives a match if any of sub-matchers match.
+     *
+     * @param mixed ...$matchers Matchers to check.
+     * @return static
+     */
+    public static function any(...$matchers): self
+    {
+        return new self(true, $matchers);
+    }
+
+    /**
+     * Get an instance of composite matcher that gives a match only if all of sub-matchers match.
+     *
+     * @param mixed ...$matchers Matchers to check.
+     * @return static
+     */
+    public static function all(...$matchers): self
+    {
+        return new self(false, $matchers);
+    }
+
+    public function match(string $path): ?bool
+    {
+        $allNulls = true;
+
+        /** @var PathMatcherInterface $matcher */
+        foreach ($this->matchers as $matcher) {
+            $match = $matcher->match($path);
+
+            if ($match === null) {
+                continue;
+            }
+
+            $allNulls = false;
+
+            if ($this->matchAny && $match) {
+                return true;
+            }
+
+            if (!$this->matchAny && !$match) {
+                return false;
+            }
+        }
+
+        return $allNulls ? null : !$this->matchAny;
+    }
+
+    private function getVariableType($variable): string
+    {
+        if (is_object($variable)) {
+            return get_class($variable);
+        }
+
+        return gettype($variable);
+    }
+}
---
+++ tests/PathMatcher/CompositeMatcherTest.php
@@ -0,0 +1,129 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Files\Tests\PathMatcher;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Files\PathMatcher\CompositeMatcher;
+use Yiisoft\Files\PathMatcher\PathMatcherInterface;
+
+
+final class CompositeMatcherTest extends TestCase
+{
+
+    public function anyProvider(): array
+    {
+        return [
+            'true-true' => [
+                [$this->getTrueMatcher(), $this->getTrueMatcher()], true,
+            ],
+            'true-false' => [
+                [$this->getTrueMatcher(), $this->getFalseMatcher()], true,
+            ],
+            'false-true' => [
+                [$this->getFalseMatcher(), $this->getTrueMatcher()], true,
+            ],
+            'false-false' => [
+                [$this->getFalseMatcher(), $this->getFalseMatcher()], false,
+            ],
+            'null-false' => [
+                [$this->getNullMatcher(), $this->getFalseMatcher()], false,
+            ],
+            'null-true' => [
+                [$this->getNullMatcher(), $this->getTrueMatcher()], true,
+            ],
+            'null-null' => [
+                [$this->getNullMatcher(), $this->getNullMatcher()], null,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider anyProvider
+     */
+    public function testAny(array $matchers, ?bool $expected): void
+    {
+        $matcher = CompositeMatcher::any(...$matchers);
+        $this->assertSame($expected, $matcher->match(''));
+    }
+
+    public function testIncorrectAnyArgument(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        CompositeMatcher::any('incorrect');
+    }
+
+    public function allProvider(): array
+    {
+        return [
+            'true-true' => [
+                [$this->getTrueMatcher(), $this->getTrueMatcher()], true,
+            ],
+            'true-false' => [
+                [$this->getTrueMatcher(), $this->getFalseMatcher()], false,
+            ],
+            'false-true' => [
+                [$this->getFalseMatcher(), $this->getTrueMatcher()], false,
+            ],
+            'false-false' => [
+                [$this->getFalseMatcher(), $this->getFalseMatcher()], false,
+            ],
+            'null-false' => [
+                [$this->getNullMatcher(), $this->getFalseMatcher()], false,
+            ],
+            'null-true' => [
+                [$this->getNullMatcher(), $this->getTrueMatcher()], true,
+            ],
+            'null-null' => [
+                [$this->getNullMatcher(), $this->getNullMatcher()], null,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider allProvider
+     */
+    public function testAll(array $matchers, ?bool $expected): void
+    {
+        $matcher = CompositeMatcher::all(...$matchers);
+        $this->assertSame($expected, $matcher->match(''));
+    }
+
+    public function testIncorrectAllArgument(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        CompositeMatcher::all('incorrect');
+    }
+
+    private function getTrueMatcher(): PathMatcherInterface
+    {
+        return new class implements PathMatcherInterface {
+            public function match(string $path): ?bool
+            {
+                return true;
+            }
+        };
+    }
+
+    private function getFalseMatcher(): PathMatcherInterface
+    {
+        return new class implements PathMatcherInterface {
+            public function match(string $path): ?bool
+            {
+                return false;
+            }
+        };
+    }
+
+    private function getNullMatcher(): PathMatcherInterface
+    {
+        return new class implements PathMatcherInterface {
+            public function match(string $path): ?bool
+            {
+                return null;
+            }
+        };
+    }
+}
